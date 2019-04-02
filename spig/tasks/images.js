09b0d98b57fffce2ed703b117a0137cbb604fb96
Igor Spasić <igor@jodd.org>
2019-04-02 11:05:04 +0200

    Added gulp-imagemin

--- spig/tasks/images.js
+++ spig/tasks/images.js
@@ -5,6 +5,7 @@ const gulp        = require('gulp');
 const parallel    = require("concurrent-transform");
 const rename      = require("gulp-rename");
 const imageResize = require('gulp-image-resize');
+const imagemin = require('gulp-imagemin');
 const SpigConfig = require('../spig-config');
 
 // creates a set of resize tasks at defined image widths
@@ -15,14 +16,17 @@ const dev = SpigConfig.devConfig;
 
 dev.resizeImageSizes.forEach(function (size) {
   let resizeImageTask = 'resize_' + size;
-  gulp.task(resizeImageTask, function(done) {
+  gulp.task(resizeImageTask, function (done) {
     gulp.src(site.srcDir + site.dirImages + '/*')
-    .pipe(parallel(
-      imageResize({ width : size }),
-      os.cpus().length
-    ))
-    .pipe(rename(function (path) { path.basename += "-" + size; }))
-    .pipe(gulp.dest(site.outDir + site.dirImages));
+      .pipe(parallel(
+        imageResize({width: size}),
+        os.cpus().length
+      ))
+      .pipe(rename(function (path) {
+        path.basename += "-" + size;
+      }))
+      .pipe(imagemin())
+      .pipe(gulp.dest(site.outDir + site.dirImages));
     done();
   });
   resizeImageTasks.push(resizeImageTask);
@@ -32,7 +36,8 @@ dev.resizeImageSizes.forEach(function (size) {
 // Copy core images to the dist folder and resize all preview images
 
 gulp.task('images', gulp.parallel(resizeImageTasks, function copyOriginalImages(done) {
-  gulp.src(site.srcDir + site.dirImages + '/*')
+  gulp.src(site.srcDir + site.dirImages + '/*/**')
+    .pipe(imagemin())
     .pipe(gulp.dest(site.outDir + site.dirImages));
-    done();
+  done();
 }));
