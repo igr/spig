ed8e3bddbbd813797311b5bc8ab583c787a83cf6
Igor Spasić <igor.spasic@gmail.com>
2019-12-17 14:07:57 +0100

    Improved slugish function!

--- spig/phase1/slugish.js
+++ spig/phase1/slugish.js
@@ -1,36 +1,68 @@
 "use strict";
 
+const _s = require('underscore.string');
 const SpigFiles = require('../spig-files');
 const Mustache = require("mustache");
 const Path = require('path');
 
+function renderSlug(slug, file) {
+  return Mustache.render(slug, SpigFiles.contextOf(file), {}, ['{', '}']);
+}
+
+function resolvePathToFileIncludingSubSlugs(file) {
+  let dirName = Path.dirname(file.out);
+
+  let dirs = dirName.split('/').slice(1);
+  let out = "/";
+  let originalOutPath = "/";
+
+  for (let dir of dirs) {
+    let slug = dir;
+    originalOutPath = originalOutPath + dir + '/';
+
+    // if there is `index` file in current folder, it can change folder name
+    const indexFile = SpigFiles.lookup(originalOutPath + 'index');
+    if (indexFile) {
+      if (indexFile.attr.slug) {
+        slug = indexFile.attr.slug;
+        slug = renderSlug(slug, indexFile);
+      }
+    }
+
+    if (slug.startsWith('/')) {
+      out = slug;
+    }
+    else {
+      out = out + slug;
+    }
+
+    if (!_s.endsWith(out, '/')) {
+      out = out + '/';
+    }
+  }
+
+  return out;
+}
+
+
 module.exports = (file) => {
+  let out = resolvePathToFileIncludingSubSlugs(file);
+
   let slug = file.attr.slug;
 
   if (!slug) {
-    const bundle = file.attr.bundle;
-    if (bundle) {
-      // special case, bundles!
-      if (file.basename !== 'index') {
-        const indexFile = SpigFiles.lookup(file.dir + 'index');
-        if (indexFile) {
-          slug = indexFile.attr.slug;
-        }
-      }
-    }
-    if (!slug) {
-      return;
-    }
+    // slug not defined on a file, return modified output path
+    file.out = out + Path.basename(file.out);
+    return;
   }
 
-  slug = Mustache.render(slug, SpigFiles.contextOf(file), {}, ['{', '}']);
+  slug = renderSlug(slug, file);
+  out = Path.dirname(out) + '/';
 
   if (slug.startsWith('/')) {
     file.out = slug + '/' + Path.basename(file.out);
-  }
-  else {
-    let dir = Path.dirname(Path.dirname(file.out));
-    file.out = dir + '/' + slug + '/' + Path.basename(file.out);
+    return;
   }
 
+  file.out = out + slug + '/' + Path.basename(file.out);
 };
