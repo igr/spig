5accebaa1ef288565762c68433c86b3ca3c43d9e
Igor Spasić <igor.spasic@gmail.com>
2019-12-17 14:06:07 +0100

    Removed unused browsers list. Added spig info to dev.

--- spig/spig-config.js
+++ spig/spig-config.js
@@ -1,9 +1,10 @@
 "use strict";
 
-const log        = require("fancy-log");
-const fs         = require("fs");
-const chalk      = require("chalk");
-const glob       = require("glob");
+const log         = require("fancy-log");
+const fs          = require("fs");
+const chalk       = require("chalk");
+const glob        = require("glob");
+const SpigVersion = require('./spig-version');
 
 const siteDefaults = {
   name: 'Spig Site',
@@ -76,20 +77,9 @@ const devDefaults = {
 
   jsUseBabel: false,
 
-  supportedBrowsers: [
-    'last 3 versions', // http://browserl.ist/?q=last+3+versions
-    'ie >= 10', // http://browserl.ist/?q=ie+%3E%3D+10
-    'edge >= 12', // http://browserl.ist/?q=edge+%3E%3D+12
-    'firefox >= 28', // http://browserl.ist/?q=firefox+%3E%3D+28
-    'chrome >= 21', // http://browserl.ist/?q=chrome+%3E%3D+21
-    'safari >= 6.1', // http://browserl.ist/?q=safari+%3E%3D+6.1
-    'opera >= 12.1', // http://browserl.ist/?q=opera+%3E%3D+12.1
-    'ios >= 7', // http://browserl.ist/?q=ios+%3E%3D+7
-    'android >= 4.4', // http://browserl.ist/?q=android+%3E%3D+4.4
-    'blackberry >= 10', // http://browserl.ist/?q=blackberry+%3E%3D+10
-    'operamobile >= 12.1', // http://browserl.ist/?q=operamobile+%3E%3D+12.1
-    'samsung >= 4', // http://browserl.ist/?q=samsung+%3E%3D+4
-  ]
+  spig: {
+    version: SpigVersion
+  }
 
 };
 
