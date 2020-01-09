afeb1b1f0ee25f5ed709915cbeb7cc93b73fe7bb
Igor Spasić <igor.spasic@gmail.com>
2020-01-09 11:14:25 +0100

    More logs

--- spig/log.js
+++ spig/log.js
@@ -4,7 +4,7 @@ const log = require('fancy-log');
 const chalk = require('chalk');
 
 module.exports.debug = (message) => {
-  log(message);
+  log(chalk.dim(message));
 };
 
 module.exports.error = (error) => {
@@ -20,9 +20,45 @@ module.exports.line = () => {
 };
 
 module.exports.banner = (version) => {
-  log(chalk.bgHex("0xF74B00").black(` -=[Spig v${version}]=- `));
+  console.log();
+  console.log(chalk.bgHex("0xF74B00").black(` -=[Spig v${version}]=- `));
+  console.log();
 };
 
 module.exports.task = (name) => {
-  log('⭐️ ' + chalk.yellow(name));
+  log('⭐️ ' + chalk.yellowBright(name));
 };
+
+module.exports.operation = (name) => {
+  log(chalk.dim('● ' + name));
+};
+
+module.exports.fileOut = (fileRef) => {
+  let outname = fileRef.spig.def.destDir + fileRef.out;
+  if (outname.startsWith('//')) {
+    outname = outname.substr(1);
+  }
+  if (fileRef.syntethic) {
+    console.log(chalk.green(outname));
+  } else {
+    console.log(chalk.green(outname) + " ⮜ " + chalk.blue(fileRef.root + fileRef.path));
+  }
+};
+
+module.exports.info = (message) => {
+  log(chalk.magentaBright(message));
+};
+
+module.exports.notification = (message) => {
+  log(chalk.cyan(message));
+};
+
+module.exports.build = (elapsedMilliseconds) => {
+  const sec = Math.floor(elapsedMilliseconds / 1000);
+  let ms = Math.floor((elapsedMilliseconds % 1000) / 10);
+  if (ms < 10) {
+    ms = '0' + ms;
+  }
+  log(chalk.white(`🔥 Site built in ${sec}.${ms} s.`));
+};
+
