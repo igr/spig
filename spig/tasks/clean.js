b033c9c963b19e5d486079b44efaa7c12ad90bd9
Igor Spasić <igor.spasic@gmail.com>
2020-01-09 09:47:50 +0100

    Added log.

--- spig/tasks/clean.js
+++ spig/tasks/clean.js
@@ -2,8 +2,10 @@
 
 const del = require('del');
 const dev = require('../spig-config').dev;
+const log = require('../log');
 
 module.exports = () => {
+  log.task("clean");
   del([
     dev.outDir + '/**/*'
   ]);
