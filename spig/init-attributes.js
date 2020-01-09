0eee6e42582de40a58592f0e2ba2839cf3f3015b
Igor Spasić <igor.spasic@gmail.com>
2020-01-09 02:11:04 +0100

    Attribute initalization.

--- spig/init-attributes.js
+++ spig/init-attributes.js
@@ -1,16 +1,64 @@
 "use strict";
 
 const _s = require("underscore.string");
-const SpigUtil = require('./spig-util');
 const Path = require('path');
+const fs = require('fs');
+const dev = require('./spig-config').dev;
 
-module.exports = (file) => {
-  let path = file.dir;
+const attrFilesCache = {};
 
-  let attr = SpigUtil.readAttributesOnPath(file, path, "__");
+function readCached(file) {
+  if (attrFilesCache[file]) {
+    return attrFilesCache[file];
+  }
+
+  attrFilesCache[file] = {};
+
+  if (fs.existsSync(file)) {
+    attrFilesCache[file] = JSON.parse(fs.readFileSync(file));
+  }
+  return attrFilesCache[file];
+}
+
+
+/**
+ * Reads attributes on path.
+ */
+function readAttributesOnPath(file, path, fileBaseName) {
+  let root = dev.srcDir + dev.dirSite;
+
+  let attr = {};
+
+  // JSON
+
+  const jsonFile = root + path + fileBaseName + '.json';
+  let config = readCached(jsonFile);
+
+  attr = {...config, ...attr};
+
+  // JS
+
+  const jsFile = root + path + fileBaseName + '.js';
+
+  if (fs.existsSync(jsFile)) {
+    const jsRelativePath = '../' + Path.relative(dev.root, Path.normalize(jsFile));
+    const jsRequireModule = jsRelativePath.substr(0, jsRelativePath.length - 3);
+    const config = require(jsRequireModule)(file);
+
+    attr = {...config, ...attr};
+  }
+
+  return attr
+};
+
+
+module.exports = (fileRef) => {
+  let path = fileRef.dir;
+
+  let attr = readAttributesOnPath(fileRef, path, "__");
 
   while (true) {
-    let config = SpigUtil.readAttributesOnPath(file, path, "_");
+    let config = readAttributesOnPath(fileRef, path, "_");
 
     attr = {...config, ...attr};
 
@@ -29,8 +77,8 @@ module.exports = (file) => {
     }
   }
 
-  // update attributes for file
+  // update attributes for file reference
 
-  file.attr = {...file.attr, ...attr};
+  fileRef.attr = {...fileRef.attr, ...attr};
 
 };
