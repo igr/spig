7df4401310cefdaf747995a1e81dbcb5843dac7a
Igor Spasić <igor.spasic@gmail.com>
2020-02-05 00:46:50 +0100

    Fixed PUG rendering.

--- src/engines/pug-engine.ts
+++ src/engines/pug-engine.ts
@@ -2,15 +2,15 @@ import pug from 'pug';
 import { RenderEngine } from './render-engine';
 
 class PugTemplateEngine implements RenderEngine<pug.Options> {
-  private readonly pubOptions: pug.Options = new (class implements pug.Options {})();
+  private readonly pugOptions: pug.Options = new (class implements pug.Options {})();
 
   configure(engineConsumer: (engine: pug.Options) => void): void {
-    engineConsumer(this.pubOptions);
+    engineConsumer(this.pugOptions);
   }
 
   render(input: string, context: object): string {
-    const compiledFunction = pug.compile(input, this.pubOptions);
-    return compiledFunction.apply(context);
+    const compiledFunction = pug.compile(input, this.pugOptions);
+    return compiledFunction(context);
   }
 
   renderInline(input: string, context: object): string {
--- src/ops/render-pug.ts
+++ src/ops/render-pug.ts
@@ -3,8 +3,12 @@ import { FileRef } from '../file-reference';
 import { PugEngine } from '../engines/pug-engine';
 
 export function renderPug(fileRef: FileRef): void {
+  let content = fileRef.string;
+
   try {
-    fileRef.string = PugEngine.render(fileRef.string);
+    content = PugEngine.render(content, fileRef.context());
+
+    fileRef.string = content;
   } catch (err) {
     log.error(`Pug render failed for ${fileRef.path}`);
     throw err;
