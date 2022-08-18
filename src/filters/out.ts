10c8376448487751f516f98e0dd7ae2f7781b48d
Igor Spasić <igor.spasic@gmail.com>
2022-08-18 10:31:44 +0200

    Fix the lint error.

--- src/filters/out.ts
+++ src/filters/out.ts
@@ -36,7 +36,8 @@ function toJSON(object: object, objectMaxDepth: number, arrayMaxLength: number, 
 
   // Matches characters that must be escaped
   // eslint-disable-next-line no-misleading-character-class,no-control-regex
-  const escapable = /[\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
+  const escapable =
+    /[\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
 
   /**
    * Escapes control characters, quote characters, backslash characters and quotes the string.
